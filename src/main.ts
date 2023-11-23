84c8f58bc7f2fb01f5a259ee92dca2ce2883348c
Raúl <ra.bhamonde@duocuc.cl>
2023-11-23 19:51:41 -0300

    com

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,69 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+// CGV-INI-1: Bibliotecas agregadas
+import { Capacitor } from '@capacitor/core';
+import { defineCustomElements as jeepSqlite} from 'jeep-sqlite/loader';
+import { APP_INITIALIZER } from '@angular/core';
+import { SqliteService } from './app/services/sqlite.service';
+import { DataBaseService } from './app/services/data-base.service';
+import { InitializeAppService } from './app/services/initialize-app.service';
+import { AuthService } from './app/services/auth.service';
+import { Storage } from '@ionic/storage-angular';
+import { ApiClientService } from './app/services/api-client.service';
+import { HttpClientModule } from '@angular/common/http';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+// CGV-FIN-1
 
 if (environment.production) {
   enableProdMode();
 }
 
+
+const platform = Capacitor.getPlatform();
+if(platform === "web") {
+  jeepSqlite(window);
+  window.addEventListener('DOMContentLoaded', async () => {
+      const jeepEl = document.createElement("jeep-sqlite");
+      document.body.appendChild(jeepEl);
+      jeepEl.autoSave = true;
+  });
+}
+
+export function initializeFactory(init: InitializeAppService) {
+  return () => init.inicializarAplicacion();
+}
+// CGV-FIN-2 
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
     provideRouter(routes),
-  ],
+    provideAnimations(),
+    // CGV-INI-3: En esta sección se agregan los servicios que hemos implementado.
+    provideIonicAngular(),
+    importProvidersFrom(IonicModule.forRoot({ innerHTMLTemplatesEnabled: true })),
+    importProvidersFrom(HttpClientModule),
+    InitializeAppService,
+    SqliteService,
+    DataBaseService,
+    AuthService,
+    Storage,
+    ApiClientService,
+    {
+        provide: APP_INITIALIZER,
+        useFactory: initializeFactory,
+        deps: [InitializeAppService],
+        multi: true
+    }
+    // CGV-FIN-3
+    ,
+    provideAnimations()
+],
 });
